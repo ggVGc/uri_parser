2525ef3463c63d3290c7a449e0232a760ba8fd06
walt <walt>
2017-10-01 18:38:09 +0200

    lua_parse basic scheme parsing

--- lua_parse.lua
+++ lua_parse.lua
@@ -14,27 +14,46 @@ fragmentSep = P'#'
 anything = P(1)
 
 query = querySep^-1 * C((anything - fragmentSep)^0)
+
 fragment = fragmentSep^-1 * C(anything^0)
+
 userOrPass = C((anything - (fragmentSep + querySep + S'@:/'))^0)
+
 userInfo = ((userOrPass * P':' * userOrPass)^1 * #P'@')^0
+
 authority = 
   Ct(userInfo)
   * P'@'^-1
   * C((anything - (querySep + fragmentSep))^1)
 
+scheme = Ct((C((anything - S':/')^1) * C(P':'^1) * C(P'/'^1))^0)
 
-patt = Ct(authority * query * fragment)
+patt = Ct(scheme * authority * query * fragment)
 
+res = match(patt, "ha:://user:pass123@www.google.com/some/path///?key=val#fragment")
 
-res = match(patt, "name:pass123@www.google.com/some/path?key=val#fragment")
+schemeRes = res[1]
+userInfoRes = res[2]
 
-for k,v in ipairs(res) do
-  if k == 1 then
-    if #v > 0 then
-      print('\tname: ', v[1])
-      print('\tpass: ', v[2])
-    end
-  else
-    print(k,v)
-  end
+if #schemeRes then
+  print('scheme => '..schemeRes[1])
+  --[[
+  print('colons => '..schemeRes[2])
+  print('slashes => '..schemeRes[3])
+  --]]
+else
+  print('scheme => null')
 end
+
+if #userInfoRes > 0 then
+  print('name => '..userInfoRes[1])
+  print('pass => '..userInfoRes[2])
+else
+  print('name => null')
+  print('pass => null')
+end
+
+print('host => '..res[3])
+print('query => '..res[4])
+print('fragment => '..res[5])
+
